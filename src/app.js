116bdaef0b13da540537b9f6743c5cae20f9102c
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-15 02:52:02 -0400

    now using ES6

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 /* jshint node: true */
+/* jshint esnext: true */
 
 'use strict';
 
@@ -8,16 +9,16 @@ var dummyData = require('./dummy.json');
 var app = express();
 
 // route for the home/root directory
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.send('<h1>Home</h1>');
 });
 
 // route for dummyData
-app.get('/Dummy', function(req, res) {
+app.get('/Dummy', (req, res) => {
     res.send(dummyData);
 });
 
 // app will listen on localhost:3000
-app.listen(3000, function() {
+app.listen(3000, () => {
     console.log('Client server running on http://localhost:3000/');
 });
