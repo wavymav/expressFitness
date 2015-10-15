2798d8f4e2b3c9614f07f08e9f1b6ca03c376f1c
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-15 02:41:54 -0400

    created dummy.json for the /Dummy route in app.js

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@
 'use strict';
 
 var express = require('express');
+var dummyData = require('./dummy.json');
 
 var app = express();
 
-// setting up the route for the home/root directory
-app.get('/', function(req, res){
+// route for the home/root directory
+app.get('/', function(req, res) {
     res.send('<h1>Home</h1>');
+});
+
+// route for dummyData
+app.get('/Dummy', function(req, res) {
+    res.send(dummyData);
 });
 
 // app will listen on localhost:3000
