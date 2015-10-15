eb640be0381e57b3702a2096b636fa6b07fb35e4
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-15 02:01:43 -0400

    created the home route

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ var express = require('express');
 
 var app = express();
 
+// setting up the route for the home/root directory
+app.get('/', function(req, res){
+    res.send('Home');
+});
+
 // app is now listening on localhost:3000
 app.listen(3000);
 console.log('Express server running on http://localhost:3000/');
