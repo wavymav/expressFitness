e32d12709ba9391afc1789289d599823cc82b8f5
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-15 02:12:45 -0400

    added callback() to the app.listen() to properly log the message

--- src/app.js
+++ src/app.js
@@ -8,9 +8,10 @@ var app = express();
 
 // setting up the route for the home/root directory
 app.get('/', function(req, res){
-    res.send('Home');
+    res.send('<h1>Home</h1>');
 });
 
 // app will listen on localhost:3000
-app.listen(3000);
-console.log('Express server running on http://localhost:3000/');
+app.listen(3000, function() {
+    console.log('Client server running on http://localhost:3000/');
+});
