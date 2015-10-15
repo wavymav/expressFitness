7fdba6953a076a9e5b68d4954f6ddcf4c205e892
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-15 01:53:37 -0400

    created express server

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 /* jshint node: true */
 
 'use strict';
+
+var express = require('express');
+
+var app = express();
+
+// app is now listening on localhost:3000
+app.listen(3000);
+console.log('Express server running on http://localhost:3000/');
